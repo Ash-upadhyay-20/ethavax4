318e6d8d7ed3e9a9105e972778c7ad16f0f91948
Ash-upadhyay-20 <99794280+Ash-upadhyay-20@users.noreply.github.com>
2023-08-22 12:13:40 +0530

    Update ethavax4.sol

--- ethavax4.sol
+++ ethavax4.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Ownable {
+    constructor() ERC20("Degen", "DGN") {
+        _mint(msg.sender, 1000000 * 0 ** decimals());
+    }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function redeemTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+    function burnTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
 
