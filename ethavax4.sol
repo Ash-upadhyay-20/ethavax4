27cca4ec4971e7a278036bbd656841ac175f49d0
Ash-upadhyay-20 <99794280+Ash-upadhyay-20@users.noreply.github.com>
2023-08-30 22:37:53 +0530

    Update ethavax4.sol

--- ethavax4.sol
+++ ethavax4.sol
@@ -5,17 +5,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
+    event PrizeRedeemed(address indexed user, uint256 amount);
+
     constructor() ERC20("Degen", "DGN") {
         _mint(msg.sender, 1000000 * 0 ** decimals());
     }
+    function redeemPrize(uint256 prizeAmount) public {
+        require(prizeAmount > 0, "Prize amount must be greater than 0");
+        require(balanceOf(msg.sender) >= prizeAmount, "Not enough tokens");
+        _burn(msg.sender, prizeAmount);
+        // Emit an event to indicate successful redemption
+        emit PrizeRedeemed(msg.sender, prizeAmount);
+    }
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-    function redeemTokens(uint256 amount) public {
-        _burn(msg.sender, amount);
     }
     function burnTokens(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 }
-
